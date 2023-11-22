728a09814bfae3bcd3afb25a2c66dd4931335cf3
akhil <akhiledakkat@gmail.com>
2023-11-22 20:21:05 +0530

    updated property listing controllers

--- API/Controllers/propertListingControllers.js
+++ API/Controllers/propertListingControllers.js
@@ -1,12 +1,13 @@
 import Listing from "../Models/listingSchema.js";
 
-
-
-export const createListing = async (req, res, next) => {
-    try {
-      const listing = await Listing.create(req.body);
-      return res.status(201).json(listing);
-    } catch (error) {
-      next(error);
-    }
-  };
+export const createListing = async (req, res) => {
+  const listing = await Listing.create(req.body);
+  if (!listing) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "couldn't create listings" });
+  }
+  return res
+    .status(201)
+    .json({ status: "sucess", message: "listing created successfully" });
+};
